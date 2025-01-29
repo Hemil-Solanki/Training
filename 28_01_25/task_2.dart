e887b7450ba833899060fe5cd6d84c9d9f608435
Coffee_UI <hemilsolanki.imscit20@gmail.com>
2025-01-29 15:37:18 +0530

    new changes

---
+++ 28_01_25/task_2.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'dart:math';
+
+main() {
+//   // -------------------------------------------
+//   print("\nGenerating Random Password.");
+//   print("Enter desired password length:");
+//   int passwordLength = int.parse(stdin.readLineSync()!);
+//
+//   String password = generatePassword(length: passwordLength);
+//   print("Generated password: $password");
+// }
+// String generatePassword({int length = 12}) {
+//   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_-+={}[]|;:<>,.?/~';
+//   final random = Random.secure();
+//   String password = '';
+//   for (int i = 0; i < length; i++) {
+//     password += chars[random.nextInt(chars.length)];
+//   }
+//   return password;
+
+  // // -------------------------------------------
+  // print("Area of Circle.");
+  // print("Enter circle radius");
+  // double radius = double.parse(stdin.readLineSync()!);
+  // double area = 3.14 * radius * radius;
+  // print("Area of circle is $area");
+
+  // // -------------------------------------------
+  // print("Program to calculate power.");
+  // print("Enter the number : ");
+  // int num = int.parse(stdin.readLineSync()!);
+  // print("Enter the power : ");
+  // int power = int.parse(stdin.readLineSync()!);
+  //
+  // int result = 1;
+  // for (int i = 1; i <= power; i++) {
+  //   result = result * num;
+  // }
+  // print("$num raise to the power of $power is $result");
+
+  // // -------------------------------------------
+  // print("Is active declaration");
+  // String? name;
+  // int? age;
+  // bool isActive = true;
+  //
+  // name = "Hemil";
+  // age = 20;
+  // print("Details : " + name + " " + age.toString() + " " + isActive.toString());
+
+  // -------------------------------------------
+//   double area1 = calculateArea();
+//   print("Area 1: $area1");
+//
+//   double area2 = calculateArea(length: 5, width: 3);
+//   print("Area 2: $area2");
+// }
+// double calculateArea({double length = 1, double width = 1}) {
+//   return length * width;
+
+
+}
